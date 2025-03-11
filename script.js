157d56a23e54fc19c21c2117e41b14ac7ce28392
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-03-11 20:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const form=document.querySelector("form");
+form.addEventListener("click",(e)=>{
+	e.preventDefault();
+	
+	const fontSize=document.getElementById("fontsize").value;
+	const fontColor=document.getElementById("fontcolor").value;
+
+	document.cookie = `fontsize=${fontSize}px `;
+    document.cookie = `fontcolor=${fontColor}`;
+	
+})
+
