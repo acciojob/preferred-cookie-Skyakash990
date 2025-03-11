6c0975de8af709e9c02ff64defe9f11e08ec34a8
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-03-11 20:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 //your JS code here. If required.
 const form=document.querySelector("form");
+const root=document.documentElement;
+
+function getCookie(name){
+	const cookies=document.cookie.split(";");
+	for(let cookie of cookies){
+		let [cookieName,cookieValue] = cookie.split("=");
+
+		if(cookieName === name){
+			return cookieValue;
+		}
+	}
+	return null;
+}
+
+const savedFontSize=getCookie("fontsize");
+const savedFontColor=getCookie("fontcolor");
+
+if(savedFontSize){
+	root.style.setProperty("--fontSize",savedFontSize);
+	document.getElementById("fontsize").value = parseInt(savedFontSize);
+}
+if(savedFontColor){
+	root.style.setProperty("--fontColor",savedFontSize);
+	document.getElementById("fontcolor").value = parseInt(savedFontColor);
+}
+
+
+
 form.addEventListener("click",(e)=>{
 	e.preventDefault();
 	
-	const fontSize=document.getElementById("fontsize").value;
+	const fontSize=document.getElementById("fontsize").value+ "px";
 	const fontColor=document.getElementById("fontcolor").value;
 
-	document.cookie = `fontsize=${fontSize}px `;
-    document.cookie = `fontcolor=${fontColor}`;
+	document.cookie = `fontsize=${fontSize}; path=/; max-age=${30 * 24 * 60 * 60}`;
+    document.cookie = `fontcolor=${fontColor}; path=/; max-age=${30 * 24 * 60 * 60}`;
+
+	root.style.setProperty("--fontsize", fontSize);
+    root.style.setProperty("--fontcolor", fontColor);
 	
 })
 
